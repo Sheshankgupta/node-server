d6fe16cc89d06536c00c57be465ea92283f79e58
sharanbrar <sharanbrar@gmail.com>
2023-10-30 18:56:58 +0530

    initial

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ limit: '500mb', extended: true, parameterLimit: 
 app.use(cors())
 // Other middleware and configurations
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('hello')
 })
 
